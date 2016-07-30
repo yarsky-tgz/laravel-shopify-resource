244c956eeef56c44d84fc9b25a00cf361d22983d
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2016-07-30 04:06:43 +0300

    session interaction

--- src/Yarsky/Shopify/Middleware/ShopifyDomain.php
+++ src/Yarsky/Shopify/Middleware/ShopifyDomain.php
@@ -9,11 +9,17 @@ class ShopifyDomain
 
     public function handle($request, Closure $next, $guard = null)
     {
-        if (!$request->has('shop') || !$request->has('hmac')) {
+        if ((!$request->has('shop') || !$request->has('hmac')) && !$request->session()->exists('shopify_domain')) {
             return $next($request);
         }
 
-        $domain = $request->input('shop');
+        if (!$request->has('shop')) {
+            $domain = session('shopify_domain');
+        } else {
+            $domain = $request->input('shop');
+            session(['shopify_domain' => $domain]);
+        }
+
         $setup['SHOP_DOMAIN'] = $domain;
         $token = \App\Token::where('domain', $domain)->first();
 

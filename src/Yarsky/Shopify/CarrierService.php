e092534348011c0002a738f4e883660b9d614dbe
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2016-08-03 14:59:03 +0300

    CarrierService class

---
+++ src/Yarsky/Shopify/CarrierService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Yarsky\Shopify;
+
+class CarrierService extends Resource
+{
+
+    const RESOURCE_NAME_MULT = 'carrier_services';
+    const RESOURCE_NAME = 'carrier_service';
+
+}

4283084052f15a538194e4398ca70b8b577b7e9e
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2016-11-27 11:17:28 +0200

    customer exists

--- src/Yarsky/Shopify/Customer.php
+++ src/Yarsky/Shopify/Customer.php
@@ -9,4 +9,14 @@ class Customer extends Resource
     use Trait/Searchable;
     const RESOURCE_NAME_MULT = 'customers';
     const RESOURCE_NAME = 'customer';
+
+    public static function exists($email)
+    {
+        $customers = static::search([
+            'query' => 'email:' . $email,
+            'fields' => 'id',
+            'limit' => 1
+        ]);
+        return (bool) $customers->count();
+    }
 }

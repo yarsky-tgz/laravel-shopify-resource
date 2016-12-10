173d0c8ad2185e9200cfc623f07fe379451cb4d6
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2016-12-10 10:10:12 +0200

    token scope

---
+++ src/Yarsky/Shopify/Scopes/TokenScope.php
@@ -0,0 +1,15 @@
+<?php
+namespace Yarsky\Shopify\Scopes;
+
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Yarsky\Shopify\Model\Token;
+
+class TokenScope implements Scope
+{
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('token', Token::current()->id);
+    }
+}
